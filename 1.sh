d0907f3088f40eab81706bd54100293f95fc09a4
bagmdgiveme@163.com <bagmdgiveme@163.com>
2017-09-14 02:42:00 -0700

    changes log for

--- 1.sh
+++ 1.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git  clone $1
-cd  `echo $1|awk -F"/" '{print $NF}'|awk -F".git" '{print $NR}'`
 git add .
 git  commit -m "changes log for `echo $1|awk -F'/' '{print $NF}'|awk -F".git" '{print $NR}'`"
 git push -u origin master
